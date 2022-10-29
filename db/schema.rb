545c2615736b538454be23d27e943993748aa519
pafev <paulofvilarimda@gmail.com>
2022-10-29 15:57:14 -0300

    dependecy between Product, Brand and Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_29_184335) do
+ActiveRecord::Schema.define(version: 2022_10_29_185418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,19 @@ ActiveRecord::Schema.define(version: 2022_10_29_184335) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "price"
+    t.integer "stock_quantity"
+    t.bigint "brand_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["brand_id"], name: "index_products_on_brand_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -46,4 +59,6 @@ ActiveRecord::Schema.define(version: 2022_10_29_184335) do
   end
 
   add_foreign_key "carts", "users"
+  add_foreign_key "products", "brands"
+  add_foreign_key "products", "categories"
 end
