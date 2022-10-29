225d468e824a86131be2b1e4e6ec04cc28c85b58
pafev <paulofvilarimda@gmail.com>
2022-10-29 12:10:22 -0300

    creating tests for User (testing name)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe User, type: :model do
   context "testing factory" do
     it {expect(build(:user)).to be_valid}
   end
+
+  context "testing name" do
+    it "name shouldn't be nil" do
+      expect(build(:user, name: nil)).to be_invalid
+    end
+    it "name should be uniq" do
+      create(:user, name: 'aa')
+      expect(build(:user, name: 'aa')).to be_invalid
+    end
+  end
 end
