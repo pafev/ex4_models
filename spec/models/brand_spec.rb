f63352d1babfec74453b306b6df34a33522bdc17
pafev <paulofvilarimda@gmail.com>
2022-10-29 11:28:21 -0300

    creating test for brand (testing factory)

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Brand, type: :model do
+  context "testing factory" do
+    it {expect(build(:brand)).to be_valid}
+  end
+  
   context "testing brand's name:" do
     it "name shouldn't be nil" do
       expect(build(:brand, name:nil)).to be_invalid
