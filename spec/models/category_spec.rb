65d3c14b77bc9928fbe99bb847350ad24443961b
pafev <paulofvilarimda@gmail.com>
2022-10-29 11:57:08 -0300

    creating test for Category (name shouldn't be nil)

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Category, type: :model do
   context "testing factory" do
     it {expect(build(:category)).to be_valid}
   end
+
+  context "testing name:" do
+    it "name shouldn't be nil" do
+      expect(build(:category, name: nil)).to be_invalid
+    end
+  end
 end
