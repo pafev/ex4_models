74cc470e529e03be1d9754e2e1707dda801e8c70
pafev <paulofvilarimda@gmail.com>
2022-10-29 11:54:41 -0300

    creating test for Category (testing factory)

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "testing factory" do
+    it {expect(build(:category)).to be_valid}
+  end
 end
