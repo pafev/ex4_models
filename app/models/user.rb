23e445f66ac496da789a50333f7beab1fb5e43f2
pafev <paulofvilarimda@gmail.com>
2022-10-29 15:51:34 -0300

    dependecy between Cart and User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_one :cart, dependent: :destroy
     validates :name, :email, presence: :true, uniqueness: :true
     validates :password, presence: :true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_29_150125) do
+ActiveRecord::Schema.define(version: 2022_10_29_184335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2022_10_29_150125) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "total_value"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -37,4 +45,5 @@ ActiveRecord::Schema.define(version: 2022_10_29_150125) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "carts", "users"
 end
