5f8f8222f5b263c5156f9b7bd01790063ff87970
pafev <paulofvilarimda@gmail.com>
2022-10-29 11:58:22 -0300

    creating test for Category (name should be uniq)

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe Category, type: :model do
     it "name shouldn't be nil" do
       expect(build(:category, name: nil)).to be_invalid
     end
+    it "name should be uniq" do
+      create(:category, name: 'aa')
+      expect(build(:category, name: 'aa')).to be_invalid
+    end
   end
 end
