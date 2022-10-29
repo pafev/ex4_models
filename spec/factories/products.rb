b7631c09d1c8bc185bc461ca5bf88d0adcba0890
pafev <paulofvilarimda@gmail.com>
2022-10-29 15:55:11 -0300

    created model Product

---
+++ spec/factories/products.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :product do
+    name { "MyString" }
+    description { "MyString" }
+    price { 1 }
+    stock_quantity { 1 }
+    brand { nil }
+    category { nil }
+  end
+end
