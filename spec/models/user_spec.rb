c0d4151d7adadcd025e0467c7fadb92c7a018fa8
pafev <paulofvilarimda@gmail.com>
2022-10-29 12:17:38 -0300

    creating tests for User (testing email)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe User, type: :model do
       expect(build(:user, name: 'aa')).to be_invalid
     end
   end
+
+  context "testing email" do
+    it "email shouldn't be nil" do
+      expect(build(:user, email: nil)).to be_invalid
+    end
+    it "email should be uniq" do
+      create(:user, name: 'aa', email:'example@email.com')
+      expect(build(:user, name: 'bb', email:'example@email.com')).to be_invalid
+    end
+  end
 end
