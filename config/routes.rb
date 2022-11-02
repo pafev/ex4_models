43636b1323488f69408da7d437fcd44eaba67ced
pafev <paulofvilarimda@gmail.com>
2022-11-01 23:26:14 -0300

    método delete (Category)

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ namespace "api" do
       get "show/:id", to: "categories#show"
       post "create", to: "categories#create"
       patch "update/:id", to: "categories#update"
+      delete "delete/:id", to: "categories#delete"
     end
   end
 end
