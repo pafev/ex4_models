22776ddb597325477d2f7b679873f8d401a78736
pafev <paulofvilarimda@gmail.com>
2022-11-04 00:00:10 -0300

    tests for index method (controller brands)

--- spec/requests/api/v1/brands_spec.rb
+++ spec/requests/api/v1/brands_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Brands", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before do
+      create(:brand, id: 1, name: 'Pandora')
+      create(:brand, id: 2, name: 'Pollo')
+      get "/api/v1/brands/index"
+    end
+    context "return http status ok" do
+      it {expect(response).to have_http_status(:ok)}
+    end
+    context "return a json" do
+      it {expect(response.content_type).to eq('application/json; charset=utf-8')}
+    end
+    context "return the created instances" do
+      it {expect(response.body).to eq(Brand.all.to_json)}
+    end
   end
 end
