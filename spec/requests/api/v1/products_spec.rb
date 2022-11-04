a832a896b37016d3d199e58f970690533cfc1a1c
pafev <paulofvilarimda@gmail.com>
2022-11-04 02:06:12 -0300

    tests for show method (controller products)

--- spec/requests/api/v1/products_spec.rb
+++ spec/requests/api/v1/products_spec.rb
@@ -19,4 +19,31 @@ RSpec.describe "Api::V1::Products", type: :request do
       it {expect(response.body).to eq(Product.all.to_json)}
     end
   end
+
+  describe "GET /show" do
+    before do
+      create(:brand, id: 1)
+      create(:category, id: 1)
+    end
+    let(:product) {create(:product, brand_id: 1, category_id: 1)}
+    context "id exists" do
+      before do
+        get "/api/v1/products/show/#{product.id}"
+      end
+      it "return http status ok" do
+        expect(response).to have_http_status(:ok)
+      end
+      it "return the correct instance" do
+        expect(response.body).to eq(product.to_json)
+      end
+    end
+    context "id doesn't exist" do
+      before do
+        get "/api/v1/products/show/-1"
+      end
+      it "return http status not_found" do
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
 end
