ed8f9b0913eefe2d2ca60565709b3a5d887e8f7f
pafev <paulofvilarimda@gmail.com>
2022-11-04 01:57:58 -0300

    tests for index method (controller products)

--- spec/requests/api/v1/products_spec.rb
+++ spec/requests/api/v1/products_spec.rb
@@ -2,6 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Products", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before do
+      create(:brand, id: 1)
+      create(:category, id: 1)
+      create(:product, name: 'Galaxy A12', brand_id: 1, category_id: 1)
+      create(:product, name: 'Moto G10', brand_id: 1, category_id: 1)
+      get "/api/v1/products/index"
+    end
+    context "return http status ok" do
+      it {expect(response).to have_http_status(:ok)}
+    end
+    context "return a json" do
+      it {expect(response.content_type).to eq('application/json; charset=utf-8')}
+    end
+    context "return the created instances" do
+      it {expect(response.body).to eq(Product.all.to_json)}
+    end
   end
 end
