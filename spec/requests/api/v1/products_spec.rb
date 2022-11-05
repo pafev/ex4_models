162518a454b4e2951f713524d7c5df64bb530d74
pafev <paulofvilarimda@gmail.com>
2022-11-05 19:31:11 -0300

    tests for create method (controller products)

--- spec/requests/api/v1/products_spec.rb
+++ spec/requests/api/v1/products_spec.rb
@@ -46,4 +46,82 @@ RSpec.describe "Api::V1::Products", type: :request do
       end
     end
   end
+
+  describe "POST /create" do
+    let(:brand) {create(:brand)}
+    let(:category) {create(:category)}
+    context "params are ok" do
+      it "return http status ok" do
+        post "/api/v1/products/create", params: {
+          product:{
+            name: "iPhone 21",
+            price: 9999999,
+            stock_quantity: 2,
+            brand_id: brand.id,
+            category_id: category.id
+            }
+          }
+        expect(response).to have_http_status(:created)
+      end
+    end
+    context "params aren't ok" do
+      it "name shouldn't be nil" do
+        post "/api/v1/products/create", params: {
+          product:{
+            name: nil,
+            price: 9999999,
+            stock_quantity: 2,
+            brand_id: brand.id,
+            category_id: category.id
+            }
+          }
+        expect(response).to have_http_status(:bad_request)
+      end
+      it "price shouldn't be nil" do
+        post "/api/v1/products/create", params: {
+          product:{
+            name: "iPhone 21",
+            price: nil,
+            stock_quantity: 2,
+            brand_id: brand.id,
+            category_id: category.id
+            }
+          }
+        expect(response).to have_http_status(:bad_request)
+      end
+      it "stock_quantity shouldn't be nil" do
+        post "/api/v1/products/create", params: {
+          product:{
+            name: "iPhone 21",
+            price: 9999999,
+            stock_quantity: nil,
+            brand_id: brand.id,
+            category_id: category.id
+            }
+          }
+        expect(response).to have_http_status(:bad_request)
+      end
+      it "name should be uniq" do
+        post "/api/v1/products/create", params: {
+          product:{
+            name: "iPhone 21",
+            price: 9999999,
+            stock_quantity: 2,
+            brand_id: brand.id,
+            category_id: category.id
+            }
+          }
+        post "/api/v1/products/create", params: {
+          product:{
+            name: "iPhone 21",
+            price: 10,
+            stock_quantity: 3500,
+            brand_id: brand.id,
+            category_id: category.id
+            }
+          }
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+  end
 end
