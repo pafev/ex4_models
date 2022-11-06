41ed93a4a1a002759f206afecd7aa865fff54abf
pafev <paulofvilarimda@gmail.com>
2022-11-06 00:28:09 -0300

    tests for create method (controller carts)

--- spec/requests/api/v1/carts_spec.rb
+++ spec/requests/api/v1/carts_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Carts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /create" do
+    context "params are ok" do
+      let(:user) {create(:user, email:"example@email")}
+      it "return http status created" do
+        post "/api/v1/carts/create", params: {cart: {user_id: user.id}}
+        expect(response).to have_http_status(:created)
+      end
+    end
+    context "params aren't ok" do
+      it "return http status bad_request" do
+        post "/api/v1/carts/create", params: nil
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
   end
 end
