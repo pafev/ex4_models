4d376ad733cd00961e1ea1486ee467bb00cea0b5
pafev <paulofvilarimda@gmail.com>
2022-11-05 23:59:52 -0300

    fixing some tests

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  let(:user) {create(:user)}
+  let(:user) {create(:user, email: "example@email")}
   context 'testing factory' do
     it {expect(build(:cart, user_id: user.id)).to be_valid}
   end
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Purchase, type: :model do
   let(:brand) {create(:brand)}
   let(:category) {create(:category)}
-  let(:user) {create(:user)}
+  let(:user) {create(:user, email: "example@email")}
   let(:product) {create(:product, brand_id: brand.id, category_id: category.id)}
   let(:cart) {create(:cart, user_id: user.id)}
   context 'testing factory' do
