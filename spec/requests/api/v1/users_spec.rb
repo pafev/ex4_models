5b639bc19940945136cdb168e8ab2a95dcd8e665
pafev <paulofvilarimda@gmail.com>
2022-11-09 16:48:30 -0300

    created tests for index method (users)

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -2,6 +2,26 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
   let(:user) {create(:user, password: 'batata')}
+  describe "GET /index" do
+    let (:user_admin) {create(:user, :admin, email: 'admin@paulo')}
+    context "user was logged in and is admin, so" do
+      before do
+        get "/api/v1/users/index", headers: {'X-User-Email': user_admin.email, 'X-User-Token': user_admin.authentication_token}
+      end
+      it "return http status ok" do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+    context "user was logged in, but isn't admin, so" do
+      before do
+        get "/api/v1/users/index", headers: {'X-User-Email': user.email, 'X-User-Token': user.authentication_token}
+      end
+      it "return http status unauthorized" do
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+  
   describe "GET /login" do
     context "when params are ok and valids" do
       it "return http status ok" do
