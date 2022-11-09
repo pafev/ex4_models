fead36d1345ee0a0167bc835d0d73a04012da4da
pafev <paulofvilarimda@gmail.com>
2022-11-09 17:31:37 -0300

    fixing user_serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :name, :is_admin, :credit_wallet, :email, :authentication_token, :profile_picture_url
-
+  attributes :id, :name, :is_admin, :credit_wallet, :email, :authentication_token, :cart, :profile_picture_url
+  
+  def cart
+    if object.cart
+      {id: object.cart.id, total_value: object.cart.total_value}
+    end
+  end
   def profile_picture_url
     rails_blob_path(object.profile_picture, only_path: true) if object.profile_picture.attached?
   end
