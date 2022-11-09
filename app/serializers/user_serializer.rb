0ba51d1c2bfc16dfc8c27eae8262e0851dc06464
pafev <paulofvilarimda@gmail.com>
2022-11-09 16:57:30 -0300

    added profile_picture in user_serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  include Rails.application.routes.url_helpers
+  attributes :id, :profile_picture_url
+
+  def profile_picture_url
+    rails_blob_path(object.profile_picture, only_path: true) if object.profile_picture.attached?
+  end
 end
