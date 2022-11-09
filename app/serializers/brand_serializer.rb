d9b014b887c89ac53140cbb911b1851e10f61393
pafev <paulofvilarimda@gmail.com>
2022-11-09 17:07:14 -0300

    adjusting brand_serializer

--- app/serializers/brand_serializer.rb
+++ app/serializers/brand_serializer.rb
@@ -1,6 +1,6 @@
 class BrandSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :logo_url
+  attributes :id, :name, :logo_url
 
   def logo_url
     rails_blob_path(object.logo, only_path: true) if object.logo.attached?
