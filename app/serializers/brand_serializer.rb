9abc9fc41a173598b8f8fcde02163f8919309d7c
pafev <paulofvilarimda@gmail.com>
2022-11-09 17:03:48 -0300

    added logo in brand_serializer

--- app/serializers/brand_serializer.rb
+++ app/serializers/brand_serializer.rb
@@ -1,3 +1,8 @@
 class BrandSerializer < ActiveModel::Serializer
-  attributes :id
+  include Rails.application.routes.url_helpers
+  attributes :id, :logo_url
+
+  def logo_url
+    rails_blob_path(object.logo, only_path: true) if object.logo.attached?
+  end
 end
