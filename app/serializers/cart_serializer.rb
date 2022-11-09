0f088bcb8a308531faa0dd5255df1243f02d7923
pafev <paulofvilarimda@gmail.com>
2022-11-09 17:34:14 -0300

    fixing cart_serializer

--- app/serializers/cart_serializer.rb
+++ app/serializers/cart_serializer.rb
@@ -1,6 +1,6 @@
 class CartSerializer < ActiveModel::Serializer
   attributes :id, :total_value, :user
   def user
-    object.user
+    {id: object.user.id, name: object.user.name, email: object.user.email, is_admin: object.user.is_admin, credit_wallet: object.user.credit_wallet}
   end
 end
