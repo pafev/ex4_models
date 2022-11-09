31bf99bd0ae83ec818226797035eb7f6519f96f3
pafev <paulofvilarimda@gmail.com>
2022-11-09 17:24:23 -0300

    adjusting serializer for users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :profile_picture_url
+  attributes :id, :name, :is_admin, :credit_wallet, :email, :authentication_token, :profile_picture_url
 
   def profile_picture_url
     rails_blob_path(object.profile_picture, only_path: true) if object.profile_picture.attached?
