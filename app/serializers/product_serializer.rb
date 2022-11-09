a47cbddda986a1d1bf13143d981d958ce691873d
pafev <paulofvilarimda@gmail.com>
2022-11-09 16:23:51 -0300

    added images in product_serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,14 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id
+  include Rails.application.routes.url_helpers
+  attributes :id, :images_url
+
+  def images_url
+    array =[]
+    if object.images.attached?
+      for image in object.images do
+        array.append(rails_blob_path(image, only_path: true))
+      end
+    end
+    array
+  end
 end
