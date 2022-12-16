8bf1e581168f095dc6fa0c10b63ebf40fe3178c8
pafev <paulofvilarimda@gmail.com>
2022-12-16 15:01:36 -0300

    adjusting tests in brand's controller

--- spec/requests/api/v1/brands_spec.rb
+++ spec/requests/api/v1/brands_spec.rb
@@ -1,31 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Brands", type: :request do
-  let(:user) {create(:user, :admin)}
+  let(:user_admin) {create(:user, :admin)}
+  let(:authentication_params_admin) {{
+    'X-User-Email': user_admin.email, 
+    'X-User-Token': user_admin.authentication_token
+    }
+  }
+  let(:user) {create(:user)}
   let(:authentication_params) {{
     'X-User-Email': user.email, 
     'X-User-Token': user.authentication_token
     }
   }
+
   describe "GET /index" do
     before do
       create(:brand, id: 1, name: 'Pandora')
       create(:brand, id: 2, name: 'Pollo')
       get "/api/v1/brands/index"
     end
+
     context "return http status ok" do
       it {expect(response).to have_http_status(:ok)}
     end
+
     context "return a json" do
       it {expect(response.content_type).to eq('application/json; charset=utf-8')}
     end
+
     context "return the created instances" do
-      it {expect(response.body).to eq("[{\"id\":1,\"name\":\"Pandora\",\"logo_url\":null},{\"id\":2,\"name\":\"Pollo\",\"logo_url\":null}]")}
+      it {expect(response.body).to eq([
+        {id: 1, name: 'Pandora', logo_url: nil},
+        {id: 2, name: 'Pollo', logo_url: nil}
+      ].to_json)}
     end
   end
 
   describe "GET /show" do
-    let(:brand) {create(:brand)}
+    let(:brand) {create(:brand, id: 1, name: 'Lacoste')}
+
     context "id exists" do
       before do
         get "/api/v1/brands/show/#{brand.id}"
@@ -33,10 +47,14 @@ RSpec.describe "Api::V1::Brands", type: :request do
       it "return http status ok" do
         expect(response).to have_http_status(:ok)
       end
+      it "return a json" do
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
       it "return the correct instance" do
-        expect(response.body). to eq("{\"id\":#{brand.id},\"name\":\"#{brand.name}\",\"logo_url\":null}")
+        expect(response.body). to eq({id: 1, name: 'Lacoste', logo_url: nil}.to_json)
       end
     end
+  
     context "id doesn't exist" do
       before do
         get "/api/v1/brands/show/-1"
@@ -49,88 +67,108 @@ RSpec.describe "Api::V1::Brands", type: :request do
 
   describe "POST /create" do
     let(:brand_params) {attributes_for(:brand)}
-    context "params are ok" do
+
+    context "params are ok and user is admin" do
       before do
-        post "/api/v1/brands/create", params: {brand: brand_params}, headers: authentication_params
+        post "/api/v1/brands/create", params: {brand: brand_params}, headers: authentication_params_admin
       end
       it "return http status created" do
         expect(response).to have_http_status(:created)
       end
-      it "brand should be uniq" do
-        post "/api/v1/brands/create", params: {brand: brand_params}, headers: authentication_params
-        expect(response).to have_http_status(:bad_request)
-      end
     end
+  
     context "params aren't ok" do
-      brand_params = nil
       before do
-        post "/api/v1/brands/create", params: {brand: brand_params}, headers: authentication_params
+        post "/api/v1/brands/create", params: {brand: nil}, headers: authentication_params_admin
       end
       it "return http status bad_request" do
         expect(response).to have_http_status(:bad_request)
+      end
+    end
+
+    context "user isn't admin" do
+      before do
+        post "/api/v1/brands/create", params: {brand: brand_params}, headers: authentication_params
+      end
+      it "return http status unauthorized" do
+        expect(response).to have_http_status(:unauthorized)
       end
     end
   end
 
   describe "PATCH /update" do
     let(:brand) {create(:brand)}
-    let(:brand2) {create(:brand, name: 'Gucci')}
-    context "id exists and params are ok" do
+  
+    context "when id exists, params are ok and user is admin" do
+      before do
+        patch "/api/v1/brands/update/#{brand.id}", params: {brand: {name: 'Pollo'}}, headers: authentication_params_admin
+      end
       it "return http status ok" do
-        patch "/api/v1/brands/update/#{brand.id}", params: {brand: {name: 'Pollo'}}, headers: authentication_params
         expect(response).to have_http_status(:ok)
       end
-      it "brand should be uniq" do
-        patch "/api/v1/brands/update/#{brand.id}", params: {brand: {name: brand2.name}}, headers: authentication_params
-        expect(response).to have_http_status(:bad_request)
+      it "updated correctly" do
+        expect(Brand.find(brand.id).name).to eq('Pollo')
       end
     end
-    context "id exists and params aren't ok" do
+  
+    context "when params aren't ok" do
       before do
-        patch "/api/v1/brands/update/#{brand.id}", params: {brand: {name: nil}}, headers: authentication_params
-      end
-      it "brand's name shouldn't be nil" do
-        expect(response).to have_http_status(:bad_request)
-      end
-    end
-    context "id doesn't exist and param are ok" do
-      before do
-        patch "/api/v1/brands/update/-1", params: {brand: {name: 'Pollo'}}, headers: authentication_params
+        patch "/api/v1/brands/update/#{brand.id}", params: {brand: nil}, headers: authentication_params_admin
       end
       it "return http status bad_request" do
         expect(response).to have_http_status(:bad_request)
       end
     end
-    context "id doesn't exist and params aren't ok" do
+  
+    context "when id doesn't exists" do
       before do
-        patch "/api/v1/brands/update/-1", params: {brand: {name: nil}}, headers: authentication_params
+        patch "/api/v1/brands/update/-1", params: {brand: {name: 'Pollo'}}, headers: authentication_params_admin
       end
       it "return http status bad_request" do
         expect(response).to have_http_status(:bad_request)
+      end
+    end
+  
+    context "when user isn't admin" do
+      before do
+        patch "/api/v1/brands/update/#{brand.id}", params: {brand: {name: 'Pollo'}}, headers: authentication_params
+      end
+      it "return http status unauthorized" do
+        expect(response).to have_http_status(:unauthorized)
       end
     end
   end
 
   describe "DELETE /delete" do
     let(:brand) {create(:brand)}
-    context "id exists" do
+  
+    context "id exists and user is admin" do
       before do
-        delete "/api/v1/brands/delete/#{brand.id}", headers: authentication_params
+        delete "/api/v1/brands/delete/#{brand.id}", headers: authentication_params_admin
       end
       it "return http status ok" do
         expect(response).to have_http_status(:ok)
       end
       it "delete instance correctly" do
-        get "/api/v1/brands/show/#{brand.id}"
-        expect(response).to have_http_status(:not_found)
+        expect(Brand.all.include?(brand)).to be_falsey
       end
     end
+  
     context "id doesn't exist" do
       before do
-        delete "/api/v1/brands/delete/-1", headers: authentication_params
+        delete "/api/v1/brands/delete/-1", headers: authentication_params_admin
       end
       it "return http status not_found" do
         expect(response).to have_http_status(:not_found)
+      end
+    end
+
+    context "user isn't admin" do
+      before do
+        delete "/api/v1/brands/delete/#{brand.id}", headers: authentication_params
+      end
+      it "return http status unauthorized" do
+        expect(response).to have_http_status(:unauthorized)
       end
     end
   end
