129132b5eddb9dd0ae7155b3579f908c5806ffa8
pafev <paulofvilarimda@gmail.com>
2022-12-15 23:44:25 -0300

    adjusting attribute is_admin from user

--- db/schema.rb
+++ db/schema.rb
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 2022_12_15_005811) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "credit_wallet", default: 0, null: false
-    t.boolean "is_admin"
+    t.boolean "is_admin", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false
